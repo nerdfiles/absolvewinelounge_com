c9375c591ddab9918eeb50cb030528e0dbfb8a5e
nerdfiles <nerdfiles@gmail.com>
2014-10-18 19:38:08 -0500

    Grunted.

---
+++ wp-content/themes/absolvewinelounge/footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * absolve-wine-lounge template for displaying the footer
+ *
+ * @package WordPress
+ * @subpackage absolve-wine-lounge
+ * @since absolve-wine-lounge 1.0
+ */
+?>
+
+				<ul class="footer-widgets"><?php
+					if ( function_exists( 'dynamic_sidebar' ) ) :
+						dynamic_sidebar( 'footer-sidebar' );
+					endif; ?>
+				</ul>
+
+			</div>
+		<?php wp_footer(); ?>
+	</body>
+</html>
