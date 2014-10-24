b6f63dada3403e779082393361b9f14a85f63ca4
nerdfiles <nerdfiles@gmail.com>
2014-10-23 23:40:11 -0500

    'Style junk.'

--- wp-content/themes/absolvution/header.php
+++ wp-content/themes/absolvution/header.php
@@ -16,43 +16,59 @@
 <!--[if IE 9]>         <html class="ie ie9" <?php language_attributes(); ?>> <![endif]-->
 <!--[if gt IE 9]><!--> <html <?php language_attributes(); ?>> <!--<![endif]-->
 
-	<head>
-		<meta charset="<?php bloginfo( 'charset' ); ?>" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<title><?php wp_title( ); ?></title>
-		<meta name="viewport" content="width=device-width" />
-		<!--[if lt IE 9]>
-			<script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.js"></script>
-		<![endif]-->
-		<?php wp_head(); ?>
-	</head>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title><?php wp_title( ); ?></title>
+    <meta name="viewport" content="width=device-width" />
+    <!--[if lt IE 9]>
+      <script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.js"></script>
+    <![endif]-->
+    <?php wp_head(); ?>
+  </head>
 
-	<body <?php body_class(); ?>>
-		<div class="site">
+  <body <?php body_class(); ?>>
+    <div class="site">
 
-			<header class="site-header">
+      <header itemscope itemtype="https://schema.org/WPHeader" class="site-header
+        <?php if ( '' != get_custom_header()->url ) : ?>
+            custom-header
+        <?php endif; ?>
+        "
+        <?php if ( '' != get_custom_header()->url ) : ?>
+        style="
 
-				<?php if ( '' != get_custom_header()->url ) : ?>
-					<img src="<?php header_image(); ?>" class="custom-header" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
-				<?php endif; ?>
+          background-image: url(<?php header_image(); ?>);
 
-				<a class="logo" href="<?php echo home_url(); ?>" title="<?php bloginfo( 'name' ); ?>">
-					<h1 class="blog-name"><?php bloginfo( 'name' ); ?></h1>
-					<div class="blog-description"><?php bloginfo( 'description' ); ?></div>
-				</a>
+          <?php if ( '' != get_custom_header()->height ) : ?>
+            __height: <?php echo get_custom_header()->height; ?>px;
+          <?php endif; ?>
 
-				<div class="menu"><?php
+          <?php if ( '' != get_custom_header()->width ) : ?>
+            __width: <?php echo get_custom_header()->width; ?>px;
+          <?php endif; ?>
 
-					$nav_menu = wp_nav_menu(
-						array(
-							'container' => 'nav',
-							'container_class' => 'main-menu',
-							'items_wrap' => '<ul class="%2$s">%3$s</ul>',
-							'theme_location' => 'main-menu',
-							'fallback_cb' => '__return_false',
-						)
-					); ?>
+        "
+        <?php endif; ?>
+      >
 
-				</div>
+        <a class="logo" href="<?php echo home_url(); ?>" title="<?php bloginfo( 'name' ); ?>">
+          <h1 class="blog-name"><?php bloginfo( 'name' ); ?></h1>
+          <div class="blog-description"><?php bloginfo( 'description' ); ?></div>
+        </a>
 
-			</header>
+        <div class="menu"><?php
+
+          $nav_menu = wp_nav_menu(
+            array(
+              'container' => 'nav',
+              'container_class' => 'main-menu',
+              'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+              'theme_location' => 'main-menu',
+              'fallback_cb' => '__return_false',
+            )
+          ); ?>
+
+        </div>
+
+      </header>
