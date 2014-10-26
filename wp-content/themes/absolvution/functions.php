bb03a39bff3ae36976a6c778ec4f646872a7291d
nerdfiles <nerdfiles@gmail.com>
2014-10-26 15:55:20 -0500

    Cleanup.

--- wp-content/themes/absolvution/functions.php
+++ wp-content/themes/absolvution/functions.php
@@ -239,3 +239,13 @@ function mytheme_comment($comment, $args, $depth) {
 	<?php endif; ?>
 <?php
 }
+
+function add_body_class( $classes )
+{
+    global $post;
+    if ( isset( $post ) ) {
+        $classes[] = $post->post_type . '-' . $post->post_name;
+    }
+    return $classes;
+}
+add_filter( 'body_class', 'add_body_class' );
