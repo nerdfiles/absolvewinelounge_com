adafa01847e6cfabb4298bb46eece27fd62741bc
nerdfiles <nerdfiles@gmail.com>
2014-10-28 15:20:23 -0500

    Some minor fix.

--- wp-content/themes/absolvution/grunt/gruntfile.js
+++ wp-content/themes/absolvution/grunt/gruntfile.js
@@ -180,7 +180,6 @@ module.exports = function(grunt) {
       test: {
         command: 'uname && pwd && cd /home2/mccabe56/www/wp && git pull -u origin master',
         options: {
-          async: false,
           showProgress: true,
           host: '<%= secret.host %>',
           username: '<%= secret.username %>',
