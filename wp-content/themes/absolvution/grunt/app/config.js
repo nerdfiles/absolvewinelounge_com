fd65797552835ad3cd9112254923f6f33285b534
nerdfiles <nerdfiles@gmail.com>
2014-10-29 06:42:38 -0500

    Cleanup.

--- wp-content/themes/absolvution/grunt/app/config.js
+++ wp-content/themes/absolvution/grunt/app/config.js
@@ -1,4 +1,28 @@
 (function() {
+  require.config({
+    paths: {
+      'components': "../bower_components",
+      'jquery': "../bower_components/jquery/dist/jquery",
+      'jquery.waypoints': "../bower_components/jquery-waypoints/waypoints.min",
+      'main': "./main"
+    },
+    shim: {
+      'jquery': {
+        'exports': 'jquery'
+      },
+      'main': {
+        'exports': 'main'
+      },
+      'jquery.waypoints': {
+        'deps': ['jquery']
+      }
+    }
+  });
 
+  if (!window.requireTestMode) {
+    require(['jquery', 'jquery.waypoints', 'main'], function(main) {
+      return window.console.log(main);
+    });
+  }
 
 }).call(this);
