07d6eda4a8998b2e45620c39d2264ce2a9c97ea2
nerdfiles <nerdfiles@gmail.com>
2014-10-29 13:32:47 -0500

    Cleanup.

--- wp-content/themes/absolvution/grunt/gruntfile.js
+++ wp-content/themes/absolvution/grunt/gruntfile.js
@@ -180,7 +180,7 @@ module.exports = function(grunt) {
 
     sshexec: {
       test: {
-        command: 'uname && pwd && cd /home2/mccabe56/www/wp && git pull -u origin master',
+        command: 'uname && pwd && cd /home2/mccabe56/www/wp && git pull -uf origin master',
         options: {
           showProgress: true,
           host: '<%= secret.host %>',
