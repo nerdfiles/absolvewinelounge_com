4c414366eca4c45ca78ad5957cb3e9d931ca80f6
nerdfiles <nerdfiles@gmail.com>
2014-11-16 00:48:57 -0600

    Cleanup.

--- wp-content/themes/absolvution/grunt/app/main.js
+++ wp-content/themes/absolvution/grunt/app/main.js
@@ -132,6 +132,8 @@
     pc = $('body.home .post-content');
     plH = parseInt(pl.height(), 8);
     pcH = parseInt(pc.height(), 8);
+    console.log(plH);
+    console.log(pcH);
     nPlH = pcH + plH;
     pl.height(nPlH);
     $('select.restrict').each(function() {
